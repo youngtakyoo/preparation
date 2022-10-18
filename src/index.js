b17be367285a40ca939518dfa990b9e8bda33bac
youngtakyoo <95837382+youngtakyoo@users.noreply.github.com>
2022-10-18 21:34:03 +0900

    add store and modules to use redux
    To manage global state set redux

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
+// store를 주입하기 위한 Provider
+import { Provider } from "react-redux";
+// 주입하기위한 store
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../redux/modules/counterSlice";
 
 const Counter = (props) => {
-  const [count, setCount] = useState(0);
-  const increase = () => {
-    setCount(count + 1);
-  };
-  const decrease = () => {
-    setCount(count - 1);
-  };
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <React.Fragment>
       <div>{count}</div>
-      <button onClick={increase}>+</button>
-      <button onClick={decrease}>-</button>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        -
+      </button>
     </React.Fragment>
   );
 };
